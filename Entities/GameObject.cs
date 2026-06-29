264231a86edc6b3b63b3fb54d77e2c4c090b1d6e
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2026-06-29 13:33:16 +0200

    Update GameObject.cs

--- Entities/GameObject.cs
+++ Entities/GameObject.cs
@@ -1,21 +1,22 @@
-using Core.Entities;
+using System.Numerics;
 
 namespace CyberEngine.Entities;
+
+public struct Transform
+{
+    public Vector3 Position;
+    public Vector3 Rotation;
+    public Vector3 Scale;
+    public static Transform Default => new Transform { Position = Vector3.Zero, Rotation = Vector3.Zero, Scale = Vector3.One };
+}
 
 public abstract class GameObject
 {
     public Transform Transform = Transform.Default;
     public float Radius { get; set; } = 0.5f;
-    
-    // Flaga oznaczająca obiekt do usunięcia na końcu ramki
     public bool IsDestroyed { get; private set; } = false;
 
     public virtual void Update(double deltaTime) { }
     public virtual void Render(GameEngine engine) { }
-
-    // Wywołaj to, zamiast manualnie modyfikować kolekcję
-    public virtual void Destroy()
-    {
-        IsDestroyed = true;
-    }
+    public virtual void Destroy() { IsDestroyed = true; }
 }
