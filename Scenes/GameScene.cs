607cf61bb29285d77edaf4130930b63f468a5ec4
dwierzbicki11 <117636569+dwierzbicki11@users.noreply.github.com>
2026-06-29 13:34:59 +0200

    Update GameScene.cs

--- Scenes/GameScene.cs
+++ Scenes/GameScene.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using Veldrid;
 using CyberEngine.Core;
 using CyberEngine.Entities;
-using System.Numerics;
 
 namespace CyberEngine.Scenes;
 
@@ -14,7 +14,6 @@ public class GameScene : Scene
     private readonly Dictionary<(int x, int z), List<Wall>> _zaladowaneChunki = new();
     
     private readonly float _cellSize = 2.0f; 
-    private readonly int _chunkSize = 15; 
 
     public override void OnLoad(GameEngine engine)
     {
@@ -60,8 +59,7 @@ public class GameScene : Scene
             if (_player.Energy < 0) _player.Energy = 0;
             engine.TriggerMuzzleFlash = true; 
 
-            Vector3 pociskDir = new Vector3(engine.CameraForward.X, engine.CameraForward.Y, engine.CameraForward.Z);
-            GameObjects.Add(new Laser(_player.Transform.Position, pociskDir)); 
+            GameObjects.Add(new Laser(_player.Transform.Position, engine.CameraForward)); 
         }
 
         if (!_trzymaneKlawisze.Contains(Key.Space) && _player.Energy < 100) _player.Energy += 1;
@@ -75,12 +73,14 @@ public class GameScene : Scene
         if (_trzymaneKlawisze.Contains(Key.A)) kierunekRuchu -= right;
         if (_trzymaneKlawisze.Contains(Key.D)) kierunekRuchu += right;
 
-        if (kierunekRuchu.LengthSquared() > 0) _player.Velocity = kierunekRuchu.Normalize() * _player.Speed;
+        if (kierunekRuchu.LengthSquared() > 0) _player.Velocity = Vector3.Normalize(kierunekRuchu) * _player.Speed;
         else _player.Velocity = Vector3.Zero;
 
         _player.Transform.Position += _player.Velocity * (float)deltaTime;
 
-        float chunkWorldSize = _chunkSize * _cellSize;
+        // Dynamiczna wielkość chunka zależąca od jakości grafiki
+        int chunkSize = SystemConfig.GraphicsQuality switch { 0 => 9, 1 => 13, 2 => 19, _ => 13 };
+        float chunkWorldSize = chunkSize * _cellSize;
         int currentChunkX = (int)MathF.Floor(_player.Transform.Position.X / chunkWorldSize);
         int currentChunkZ = (int)MathF.Floor(_player.Transform.Position.Z / chunkWorldSize);
 
@@ -104,19 +104,16 @@ public class GameScene : Scene
             if (!_zaladowaneChunki.ContainsKey(coord))
             {
                 List<Wall> scianyChunku = new List<Wall>();
-                bool[,] grid = GenerujSektorMatematyczny(coord.x, coord.z, _chunkSize);
+                bool[,] grid = GenerujSektorMatematyczny(coord.x, coord.z, chunkSize);
 
-                for (int x = 0; x < _chunkSize; x++)
+                for (int x = 0; x < chunkSize; x++)
                 {
-                    for (int z = 0; z < _chunkSize; z++)
+                    for (int z = 0; z < chunkSize; z++)
                     {
                         if (grid[x, z])
                         {
-                            float worldX = (coord.x * _chunkSize + x) * _cellSize;
-                            float worldZ = (coord.z * _chunkSize + z) * _cellSize;
-                            Wall w = new Wall(worldX, worldZ, _cellSize);
-                            scianyChunku.Add(w);
-                            GameObjects.Add(w);
+                            Wall w = new Wall((coord.x * chunkSize + x) * _cellSize, (coord.z * chunkSize + z) * _cellSize, _cellSize);
+                            scianyChunku.Add(w); GameObjects.Add(w);
                         }
                     }
                 }
@@ -124,7 +121,6 @@ public class GameScene : Scene
             }
         }
 
-        // Optymalizacja CPU: Kolizje z użyciem dystansu do kwadratu
         for (int i = 0; i < GameObjects.Count; i++)
         {
             if (GameObjects[i] is Wall wall)
@@ -136,9 +132,9 @@ public class GameScene : Scene
 
                 if (distSq < (reqDist * reqDist) && distSq > 0.001f)
                 {
-                    float odleglosc = MathF.Sqrt(distSq); // Pierwiastek tylko przy samej kolizji
+                    float odleglosc = MathF.Sqrt(distSq);
                     float overlap = reqDist - odleglosc;
-                    _player.Transform.Position += new MyEngine.Core.Math.Vector3((dx / odleglosc) * overlap, 0f, (dz / odleglosc) * overlap);
+                    _player.Transform.Position += new Vector3((dx / odleglosc) * overlap, 0f, (dz / odleglosc) * overlap);
                 }
 
                 for (int j = 0; j < GameObjects.Count; j++)
@@ -147,9 +143,8 @@ public class GameScene : Scene
                     {
                         float ldx = laser.Transform.Position.X - wall.Transform.Position.X;
                         float ldz = laser.Transform.Position.Z - wall.Transform.Position.Z;
-                        float lDistSq = ldx * ldx + ldz * ldz;
-                        float lReqDist = laser.Radius + wall.Radius;
-                        if (lDistSq < (lReqDist * lReqDist)) laser.Destroy();
+                        if ((ldx * ldx + ldz * ldz) < ((laser.Radius + wall.Radius) * (laser.Radius + wall.Radius))) 
+                            laser.Destroy();
                     }
                 }
             }
@@ -159,12 +154,9 @@ public class GameScene : Scene
     private bool[,] GenerujSektorMatematyczny(int cx, int cz, int size)
     {
         bool[,] grid = new bool[size, size];
-        for (int x = 0; x < size; x++)
-            for (int z = 0; z < size; z++) grid[x, z] = true;
-
+        for (int x = 0; x < size; x++) for (int z = 0; z < size; z++) grid[x, z] = true;
         int seed = (cx * 73856093) ^ (cz * 83492791);
         Random rng = new Random(seed);
-
         Stack<(int x, int z)> stack = new Stack<(int, int)>();
         grid[1, 1] = false; stack.Push((1, 1));
 
@@ -172,20 +164,16 @@ public class GameScene : Scene
         {
             var curr = stack.Peek();
             List<(int x, int z)> neighbors = new List<(int, int)>();
-
             if (curr.x - 2 > 0 && grid[curr.x - 2, curr.z]) neighbors.Add((curr.x - 2, curr.z));
             if (curr.x + 2 < size - 1 && grid[curr.x + 2, curr.z]) neighbors.Add((curr.x + 2, curr.z));
             if (curr.z - 2 > 0 && grid[curr.x, curr.z - 2]) neighbors.Add((curr.x, curr.z - 2));
             if (curr.z + 2 < size - 1 && grid[curr.x, curr.z + 2]) neighbors.Add((curr.x, curr.z + 2));
 
-            if (neighbors.Count > 0)
-            {
+            if (neighbors.Count > 0) {
                 var next = neighbors[rng.Next(neighbors.Count)];
                 grid[curr.x + (next.x - curr.x) / 2, curr.z + (next.z - curr.z) / 2] = false;
-                grid[next.x, next.z] = false;
-                stack.Push(next);
-            }
-            else stack.Pop();
+                grid[next.x, next.z] = false; stack.Push(next);
+            } else stack.Pop();
         }
 
         int mid = size / 2;
